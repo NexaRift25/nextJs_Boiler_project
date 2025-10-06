b001ca7cb323d072216108d77b55b42c93cee2a7
Md. Mosaraf Hossain Nissan <151007488+nissandev@users.noreply.github.com>
2025-10-07 02:46:26 +0600

    setup nextjs boiler plate

---
+++ app/test/page.tsx
@@ -0,0 +1,35 @@
+    export default function Test() {
+        return <div>
+
+        <div className='grid grid-cols-4'>
+        <div className=''>
+                <div className="bg-primary font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+                    <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+                        <h1 className="text-2xl font-bold text-smoky-white">Test</h1>
+                    </main>
+                </div >
+            </div>
+            <div className='theme-soft-dark'>
+                <div className="bg-primary font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+                    <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+                        <h1 className="text-2xl font-bold text-smoky-white">Test</h1>
+                    </main>
+                </div >
+            </div>
+            <div className='theme-pink'>
+                <div className="bg-primary font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+                    <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+                    <h1 className="text-2xl font-bold text-smoky-white">Test</h1>
+                    </main>
+                </div >
+            </div>
+            <div className='theme-green'>
+                <div className="bg-primary font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+                    <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+                        <h1 className="text-2xl font-bold text-smoky-white">Test</h1>
+                    </main>
+                </div >
+            </div>
+        </div>
+        </div>
+    }
